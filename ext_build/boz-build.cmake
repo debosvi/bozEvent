e7a3a4140a4eb55114dc5baf6b429385343fb2d6
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2015-06-24 21:55:13 +0200

    Install all headers

--- ext_build/boz-build.cmake
+++ ext_build/boz-build.cmake
@@ -9,13 +9,14 @@ link_directories (${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
 
 set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON) 
 include(${CMAKE_CURRENT_LIST_DIR}/boz-vars.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/boz-install.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/boz-incgen.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/boz-doc.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/boz-install.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/boz-test-macros.cmake)
 
 include_directories (${HEADER_INSTALL_DIR})
 link_directories (${LIBRARY_INSTALL_DIR})
+link_directories (${LIBRARY_INSTALL_DIR}/static)
 
 macro(add_s6service svc_name svc_proc_name)
   message(STATUS "No process to supervise with project component ${svc_name}")
